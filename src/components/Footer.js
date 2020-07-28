ad4bb2575b2a56fb7c48e0d1191ff5c122d50d55
Olusegun Faith Emmanuel <51266654+propenster@users.noreply.github.com>
2020-07-28 02:41:07 +0100

    Added Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+return (
+<div className="container">
+<footer className="footer">
+ <div className="row"> Made with React JS<br />
+     &copy; 2020 by propenster
+</div>
+      </footer>
+</div>
+
+
+)
+
+}
